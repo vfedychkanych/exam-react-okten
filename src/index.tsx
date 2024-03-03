c5d69905ba565d29f470169e25c01ede1c2080de
Vitalii Fedychkanych <124129674+vfedychkanych@users.noreply.github.com>
2024-03-04 01:16:18 +0200

    final version

--- src/index.tsx
+++ src/index.tsx
@@ -7,8 +7,7 @@ import {router} from "./router";
 
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+  document.getElementById('root') as HTMLElement);
 root.render(
   <RouterProvider router={router}/>
 );
